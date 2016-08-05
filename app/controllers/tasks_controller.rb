963d1af51e04dd343e63162ff6bdfca634f78f42
Flipez <github@flipez.net>
2016-08-05 20:16:54 +0200

    do not render if xhr
    Signed-off-by: Flipez <github@flipez.net>

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -41,12 +41,17 @@ class TasksController < ApplicationController
   # PATCH/PUT /tasks/1.json
   def update
     respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
+        unless request.xhr?
+          format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+          format.json { render :show, status: :ok, location: @task }
+        else
+          return :ok
+        end
       else
-        format.html { render :edit }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
+        unless request.xhr?
+          format.html { render :edit }
+          format.json { render json: @task.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
