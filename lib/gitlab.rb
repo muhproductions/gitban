17464e103b13058a68ae0f7a24df599491a2c0f9
Tim Foerster <github@mailserver.1n3t.de>
2016-08-05 17:50:33 +0200

    Basic gitlab interactions to load issues and projects.
    Signed-off-by: Tim Foerster <github@mailserver.1n3t.de>

---
+++ lib/gitlab.rb
@@ -0,0 +1,53 @@
+class Gitlab 
+
+  attr_accessor :token, :api_url
+
+  def initialize token: nil, api_url: nil
+    @token, @api_url = token, api_url
+  end
+
+  def groups
+    @groups ||= begin 
+                  r = Typhoeus::Request.new "#{api_url}/groups", 
+                    params: { private_token: token }
+                  logger.debug "Gitlab - Loading groups ..."
+                  load_json r.run
+                end
+  end
+
+  def projects
+    @projects ||= begin 
+                   r = Typhoeus::Request.new "#{api_url}/projects/all", 
+                     params: { private_token: token }
+                   logger.debug "Gitlab - Loading projects ..."
+                   load_json r.run
+                 end
+  end
+
+  def issues 
+    @issues ||= begin 
+                  groups.flat_map do |group|
+                    r = Typhoeus::Request.new "#{api_url}/groups/#{group['id']}/issues", 
+                      params: { private_token: token }
+                    logger.debug "Gitlab - Loading issues over groups ..."
+                    load_json r.run
+                  end
+                end
+  end
+
+  private
+
+  def load_json r
+    if r.code.between? 200, 299
+      Oj.load r.body
+    else
+      logger.error "Gitlab - request failed with code: #{r.code}"
+      nil
+    end
+  end
+
+  def logger
+    Rails.logger
+  end
+
+end
