400d2042d8ace21d2184b154300b368b02bed46b
Flipez <github@flipez.net>
2016-08-05 21:03:22 +0200

    fix typo
    Signed-off-by: Flipez <github@flipez.net>

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :milestone, optional: true
-  belongs_to :assigne, optional: true
+  belongs_to :assignee, optional: true
   belongs_to :project, optional: true
   belongs_to :column, optional: true
 
