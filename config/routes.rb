2c8d2a1fd7c88e285e7dbcdb28a860e9adb9a663
Flipez <github@flipez.net>
2016-08-05 20:18:00 +0200

    add welcome page route
    Signed-off-by: Flipez <github@flipez.net>

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   resources :milestones
   resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
+  root to: 'welcome#index'
 end
