1e555841b3503d314114c6bed9f1bade51c10960
Tim Foerster <github@mailserver.1n3t.de>
2016-08-05 20:10:56 +0200

    Gitlab api requires pagination support. Add merge requests.
    Signed-off-by: Tim Foerster <github@mailserver.1n3t.de>

--- lib/gitlab.rb
+++ lib/gitlab.rb
@@ -10,35 +10,51 @@ class Gitlab
   end
 
   def groups
-    @groups ||= begin 
-                  r = Typhoeus::Request.new "#{api_url}/groups", 
-                    params: { private_token: token }
-                  logger.debug "Gitlab - Loading groups ..."
-                  load_json r.run
-                end
+    @groups ||= api('groups') 
   end
 
   def projects
-    @projects ||= begin 
-                   r = Typhoeus::Request.new "#{api_url}/projects/all", 
-                     params: { private_token: token }
-                   logger.debug "Gitlab - Loading projects ..."
-                   load_json r.run
-                 end
+    @projects ||= api("projects/all")
   end
 
   def issues 
-    @issues ||= begin 
-                  groups.flat_map do |group|
-                    r = Typhoeus::Request.new "#{api_url}/groups/#{group['id']}/issues", 
-                      params: { private_token: token }
-                    logger.debug "Gitlab - Loading issues over groups ..."
-                    load_json r.run
-                  end
-                end
+    @issues ||= groups.flat_map do |group|
+      api "groups/#{group['id']}/issues" 
+    end.compact
   end
 
-  private
+  def merge_requests 
+    @mr ||= projects.flat_map do |project|
+      api "projects/#{project['id']}/merge_requests"
+    end.compact
+  end
+
+  def flush
+    @mr, @issues, @projects, @groups = []
+  end
+
+  def api path
+    api_return = []
+    hydra = Typhoeus::Hydra.new
+    r = Typhoeus::Request.new(
+      "#{api_url}/#{path}", 
+      params: { private_token: token }
+    )
+    api_return << load_json(r.run)
+    requests = []
+    (r.response.headers["X-Total-Pages"].to_i - 1).times do |i|
+      re = Typhoeus::Request.new("#{api_url}/#{path}", 
+                                 params: { 
+                                   private_token: token,
+                                   page: i + 2 
+                                 })
+      requests << re
+      hydra.queue re
+    end
+    hydra.run
+    api_return << requests.map{ |r| load_json(r.response) }.compact
+    api_return.flatten
+  end
 
   def load_json r
     if r.code.between? 200, 299
