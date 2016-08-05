aba87a7bbab3e14042bb1bf2358029e16ce93b3f
Flipez <github@flipez.net>
2016-08-05 20:16:00 +0200

    resort tasks if list was updated
    Signed-off-by: Flipez <github@flipez.net>

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,28 @@ class Task < ApplicationRecord
   belongs_to :assigne, optional: true
   belongs_to :project, optional: true
   belongs_to :column, optional: true
+
+  before_save :resort
+
+  default_scope { order(:position) }
+
+  private
+
+  def resort
+    Task.transaction do
+      self.column.tasks.each_with_index do |task, i|
+        next if task.id == self.id
+        i += 1 if i >= self.position
+        task.update_column :position, i
+      end
+      if column_id_changed?
+        Task.transaction do
+          Column.find(changed_attributes[:column_id]).tasks.each_with_index do |task, i|
+            next if task.id == self.id
+            task.update_column :position, i
+          end
+        end
+      end
+    end
+  end
 end
