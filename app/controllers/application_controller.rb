099ed8237d8bab242cd4bbcdfb385f76f0e18fc2
Flipez <code@brauser.io>
2016-10-25 13:41:09 +0200

    Show warning if profile is incomplete
    Signed-off-by: Flipez <code@brauser.io>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action do
     @boards = Board.all
+    @current_user = current_user
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,8 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0,20]
     end
   end
+
+  def setup_complete?
+    !(self.token.nil? || self.token.empty?)
+  end
 end
