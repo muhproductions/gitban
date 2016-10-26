63679d59d72e7de9175db1575942fb475543e2bd
Flipez <code@brauser.io>
2016-10-26 15:35:38 +0200

    Another attempt to fix protect_from_forgery
    Signed-off-by: Flipez <code@brauser.io>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :reset_session
+  protect_from_forgery with: :exception
 
   before_action :authenticate_user!
   before_action do
